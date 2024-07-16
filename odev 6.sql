6382d467c849297a23935d1fee81ff45089a81ff
kaybedenlerdenbiri <garwiss17@gmail.com>
2024-07-16 03:35:20 +0300

    odev 6 yapıldı

---
+++ odev 6.sql
@@ -0,0 +1,17 @@
+/*
+SELECT ROUND(AVG(rental_rate),4) FROM film
+*/
+/*
+SELECT COUNT(title) FROM film
+WHERE title ~~ 'C%'
+*/
+
+/*
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99
+*/
+
+/*
+SELECT COUNT(length)FROM film
+WHERE length > 150
+*/
